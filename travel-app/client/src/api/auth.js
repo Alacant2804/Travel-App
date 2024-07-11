c8c069c1ab1e4524e8edb8cf314ab6854d902525
Alacant2804 <128549224+Alacant2804@users.noreply.github.com>
2024-07-10 20:03:09 -0500

    Set up for Vercel

--- travel-app/client/src/api/auth.js
+++ travel-app/client/src/api/auth.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 // /auth specifies that the routes under this path are related to authentication
 
 // Base URL for the authentication API
-const API_URL = 'http://localhost:5001/api/auth';
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5001/api/auth';
 
 // Register a new user
 const register = async (userData) => {
